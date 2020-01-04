3819ecec5a663f6e5bf3ec0571fd981f6f5968c2
Kizito Njoku <nkizito57@gmail.com>
2020-01-04 19:00:41 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,13 +4,16 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    Song.all.first.genre
   end
 
   def song_count
     #return the number of songs associated with the artist
+    Song.all.size
   end
 
   def genre_count
     #return the number of genres associated with the artist
+    Genre.all.size
   end
 end
